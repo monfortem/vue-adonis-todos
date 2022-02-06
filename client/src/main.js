a471d1f547499f0794f4b1f1034dd5fa29d18f00
monfortem <michael@hercules.local>
2022-02-06 16:06:01 -0500

    initial commit

--- client/src/main.js
+++ client/src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { sync } from 'vuex-router-sync'
 
 Vue.config.productionTip = false
+
+Vue.use(vuetify)
+sync(store, router)
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
   state: {
   },
   mutations: {
@@ -11,5 +13,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [createPersistedState()]
 })
