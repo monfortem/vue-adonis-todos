1e74da7c67f8257e5be8b918dc515806daed485a
monfortem <michael@hercules.local>
2022-02-12 13:23:53 -0500

    edit and delete projects

--- client/src/components/Projects.vue
+++ client/src/components/Projects.vue
@@ -1,17 +1,52 @@
 <template>
   <Panel title="Projects">
     <v-container fluid>
-      <v-row class="pa-0 ma-0">
-        <v-col>
-          <div v-for="project in projects" :key="project.id">
-            {{project.title}}
-          </div>
-        </v-col>
-      </v-row>
+      <span v-if="projects.length === 0" class="text--disabled">Add a new project...</span>
+      <div v-for="project in projects" :key="project.id">
+        <v-row class="pa-0 ma-0">
+          <v-col md="10" class="text-body-2">
+              <v-text-field
+                autofocus
+                v-if="project.isEditMode"
+                :value="project.title"
+                @keyup.enter="updateProject(project)"
+                @input="setProjectTitle({
+                  project,
+                  title: $event
+                })">
+              </v-text-field>
+              <span v-else>{{project.title}}</span>
+          </v-col>
+          <v-col md="2" class="d-flex align-center justify-center">
+            <v-btn v-if="!project.isEditMode"
+              icon
+              color="green"
+              @click="setIsEditMode(project)"
+            >
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+            <v-btn v-else
+              icon
+              color="green"
+              @click="updateProject(project)"
+            >
+              <v-icon>mdi-check</v-icon>
+            </v-btn>
+            <v-btn
+              icon
+              color="green"
+              @click="deleteProject(project)"
+            >
+              <v-icon>mdi-trash-can</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </div>
       <v-row>
         <v-col md="10">
           <v-text-field
             placeholder="My project name..."
+            @keyup.enter="createProject"
             @input="setNewProjectTitle"
             :value="newProjectTitle"
           ></v-text-field>
@@ -31,21 +66,34 @@
 </template>
 
 <script>
-import { mapActions, mapMutations, mapState } from 'vuex'
+import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
 
 export default {
+  mounted () {
+    if (this.isLoggedIn) {
+      this.fetchProjects()
+    }
+  },
   computed: {
     ...mapState('projects', [
       'newProjectTitle',
       'projects'
+    ]),
+    ...mapGetters('authentication', [
+      'isLoggedIn'
     ])
   },
   methods: {
     ...mapMutations('projects', [
-      'setNewProjectTitle'
+      'setNewProjectTitle',
+      'setIsEditMode',
+      'setProjectTitle'
     ]),
     ...mapActions('projects', [
-      'createProject'
+      'createProject',
+      'fetchProjects',
+      'updateProject',
+      'deleteProject'
     ])
   }
 }
--- client/src/store/projects.js
+++ client/src/store/projects.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 // import router from '../router'
 import HTTP from '../http'
 
@@ -16,6 +17,24 @@ export default {
           commit('appendProject', data)
           commit('setNewProjectTitle', null)
         })
+    },
+    fetchProjects ({ commit }) {
+      return HTTP().get('/projects')
+        .then(({ data }) => {
+          commit('setProjects', data)
+        })
+    },
+    updateProject ({ commit }, project) {
+      return HTTP().patch(`/projects/${project.id}`, project)
+        .then(() => {
+          commit('unsetIsEditMode', project)
+        })
+    },
+    deleteProject ({ commit }, project) {
+      return HTTP().delete(`/projects/${project.id}`)
+        .then(() => {
+          commit('deleteProject', project)
+        })
     }
   },
   getters: {
@@ -25,8 +44,28 @@ export default {
     setNewProjectTitle (state, title) {
       state.newProjectTitle = title
     },
+    setProjectTitle (state, { project, title }) {
+      project.title = title
+    },
     appendProject (state, project) {
+      const tmpProjects = state.projects
+      state.projects = []
       state.projects.push(project)
+      tmpProjects.forEach(prj => {
+        state.projects.push(prj)
+      })
+    },
+    deleteProject (state, project) {
+      state.projects.splice(state.projects.indexOf(project), 1)
+    },
+    setProjects (state, projects) {
+      state.projects = projects
+    },
+    setIsEditMode (state, project) {
+      Vue.set(project, 'isEditMode', true)
+    },
+    unsetIsEditMode (state, project) {
+      project.isEditMode = false
     }
   }
 }
